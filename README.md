848f2b44c4d738207d550c76a7e4b43348e1c50c
Gaetan Semet <gaetan@xeberon.net>
2018-10-07 22:01:38 +0200

    Adapt for Rpi3B+ and epd2in13b

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Pihole Display
 
+
+![Pi Hole stats](pihole-stat-screenshot.jpg)
+
 ## Hardware
-  - Raspberry Pi zero with pihole
-  - Waveshare 2.13 inch ePaper display (I got mine from [here](https://www.aliexpress.com/item/250x122-2-13inch-E-Ink-display-HAT-for-Raspberry-Pi-supports-partial-refresh/32812115703.html))
+  - Raspberry Pi 3B+ with pihole
+  - Waveshare 2.13 inch ePaper display model B (3 colors: black, white and red)
 ## Installation
 ### Waveshare display drivers
   - Install the libraries mentioned [here](https://www.waveshare.com/wiki/Pioneer600#Libraries_Installation_for_RPi).
@@ -11,10 +14,6 @@
 ### Font Installation
   - to install the font on your Pi, run
     ~~~~
-    cd ~
-    wget http://kottke.org/plus/type/silkscreen/download/silkscreen.zip
-    unzip silkscreen.zip
-    ~~~~
   - also install the requests library (needs pip to be installed)
     ~~~~
     sudo pip install requests
@@ -22,10 +21,11 @@
 ### Final steps
   - If you downloaded the demo code, just place the stats.py inside the 2.13inch_e-Paper_HAT-code/raspberrypi/python folder
   - If you didnot download the demo code, copy the python folder from this repo into your pi folder.
-  - run the stats.py script using 
+  - run the stats.py script using
     ~~~~
-    sudo python stats.py
+    ./start-pihole-stat.sh
     ~~~~
 ### Running on startup
-  - Lastly we just want to make this run at boot. We'll do that the easy way by editing /etc/rc.local with sudo nano /etc/rc.local and adding sudo python ~pi/stats.py & before exit 0
+  - Lastly we just want to make this run at boot. We'll do that the easy way by editing
+    `/etc/rc.local` adding `/home/pi/piholedisplay/start-pihole-stat.sh &` before `exit 0`.
   - Then save and you can reboot to test it out
--- python/stats.py
+++ python/stats.py
@@ -1,80 +1,138 @@
-import epd2in13
+#!/usr/bin/env python
+
+ #  @filename   :   main.cpp
+ #  @brief      :   2.13inch e-paper display demo
+ #  @author     :   Yehui from Waveshare
+ #
+ #  Copyright (C) Waveshare     September 9 2017
+ #
+ # Permission is hereby granted, free of charge, to any person obtaining a copy
+ # of this software and associated documnetation files (the "Software"), to deal
+ # in the Software without restriction, including without limitation the rights
+ # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ # copies of the Software, and to permit persons to  whom the Software is
+ # furished to do so, subject to the following conditions:
+ #
+ # The above copyright notice and this permission notice shall be included in
+ # all copies or substantial portions of the Software.
+ #
+ # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ # FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ # LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ # THE SOFTWARE.
+ ##
+
+import epd2in13b
 import time
 import Image
 import ImageDraw
 import ImageFont
-import json
-import subprocess
-import time
+import PIL
+from textwrap import dedent
 import requests
+import subprocess
+import json
 
-api_url = 'http://pi.hole/admin/api.php'
-bg_color = 0
-fill_color = 255
+api_url = 'http://localhost/admin/api.php'
 
-epd = epd2in13.EPD()
-epd.init(epd.lut_partial_update)
+def deep_reset(epd):
+    print "resetting to white..."
+    white_screen = Image.new('1', (epd2in13b.EPD_WIDTH, epd2in13b.EPD_HEIGHT), 255)
+    epd.display_frame(epd.get_frame_buffer(white_screen), epd.get_frame_buffer(white_screen))
+    epd.delay_ms(1000)
 
-# For simplicity, the arguments are explicit numerical coordinates
-image = Image.new('1', (epd2in13.EPD_WIDTH, epd2in13.EPD_HEIGHT), 255)  # 255: clear the frame
-draw = ImageDraw.Draw(image)
+def update(epd):
 
-width = epd2in13.EPD_WIDTH
-height = epd2in13.EPD_HEIGHT
+    # EPD 2 inch 13 b HAT is rotated 90 clockwize and does not support partial update
+    # But has amazing 2 colors
+    print "drawing status"
+    width = epd2in13b.EPD_HEIGHT
+    height = epd2in13b.EPD_WIDTH
+    top = 2
+    fill_color = 0
+    xt = 70
+    xc = 120
+    xc2 = 164
 
-draw.rectangle((0, 0, width, height), outline=0, fill=0)
+    while True:
+        frame_black = Image.new('1', (width, height), 255)
+        frame_red = Image.new('1', (width, height), 255)
 
-padding = -2
-top = padding
-bottom = height - padding
+        pihole_logo_top = Image.open('pihole-bw-80-top.bmp')
+        pihole_logo_bottom = Image.open('pihole-bw-80-bottom.bmp')
+        # pihole_logo = Image.open('monocolo    r.bmp')
 
-x = 0
+        font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 10)
+        font_bold = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf', 11)
+        font_title = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf', 11)
+        font_title_bold = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSansMono-Bold.ttf', 11)
+        # font = ImageFont.truetype('slkscr.ttf', 15)
+        draw_black = ImageDraw.Draw(frame_black)
 
-epd.set_frame_memory(image, 0, 0)
-epd.display_frame()
-epd.set_frame_memory(image, 0, 0)
-epd.display_frame()
+        draw_black.rectangle((0, 0, width, height), outline=0, fill=None)
 
-while True:
-	cmd = "hostname -I | cut -d\' \' -f1"
-	IP = subprocess.check_output(cmd, shell=True)
-	cmd = "hostname"
-	HOST = subprocess.check_output(cmd, shell=True)
-	cmd = "top -bn1 | grep load | awk " \
-	"'{printf \"CPU Load: %.2f\", $(NF-2)}'"
-	CPU = subprocess.check_output(cmd, shell=True)
-	cmd = "free -m | awk 'NR==2{printf " \
-	"\"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-	MemUsage = subprocess.check_output(cmd, shell=True)
-	cmd = "df -h | awk '$NF==\"/\"{printf " \
-	"\"Disk: %d/%dGB %s\", $3,$2,$5}'"
-	Disk = subprocess.check_output(cmd, shell=True)
-	
-	try:
-		r = requests.get(api_url)
-		data = json.loads(r.text)
-		DNSQUERIES = data['dns_queries_today']
-		ADSBLOCKED = data['ads_blocked_today']
-		CLIENTS = data['unique_clients']
-	except KeyError:
-		time.sleep(1)
-		continue
+        ip = subprocess.check_output( "hostname -I | cut -d' ' -f1", shell=True).strip()
+        print "ip:", ip
+        host = subprocess.check_output("hostname", shell=True).strip() + ".local"
+        print "host:", host
+        mem_usage = subprocess.check_output(dedent("""
+            free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'
+            """).strip(), shell=True).replace("Mem: ", "")
+        print "memory usage:", mem_usage
+        disk = subprocess.check_output(dedent("""
+            df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'
+            """).strip(), shell=True).replace("Disk: ", "")
+        print "disk:", disk
 
-	time_image = Image.new('1', (height, width), bg_color)  # 255: clear the frame
-	draw = ImageDraw.Draw(time_image)
-	font = ImageFont.truetype("/home/pi/slkscr.ttf", 15)
-	
-	draw.text((x, top), "HOST: " + HOST, font=font, fill=fill_color)
-	draw.text((x, top + 15), "IP: " + str(IP), font=font, fill=fill_color)
-	draw.text((x, top + 30),     str(CPU), font=font, fill=fill_color)
-	draw.text((x, top + 45),    str(MemUsage),  font=font, fill=fill_color)
-	draw.text((x, top + 60),    str(Disk),  font=font, fill=fill_color)
-	draw.text((x, top + 75), "Ads Blocked: " + str(ADSBLOCKED), font=font, fill=fill_color)
-	draw.text((x, top + 90), "Clients:     " + str(CLIENTS), font=font, fill=fill_color)
-	draw.text((x, top + 105), "DNS Queries: " + str(DNSQUERIES), font=font, fill=fill_color)
-	
-	epd.set_frame_memory(time_image.transpose(Image.ROTATE_90), 0, 0)
-	epd.display_frame()
-	epd.set_frame_memory(time_image.transpose(Image.ROTATE_90), 0, 0)
-	epd.display_frame()
-	time.sleep(1)
+        try:
+            r = requests.get(api_url)
+            data = json.loads(r.text)
+            dnsqueries = data['dns_queries_today']
+            adsblocked = data['ads_blocked_today']
+            clients = data['unique_clients']
+        except KeyError:
+            time.sleep(1)
+            continue
+
+        frame_black.paste(pihole_logo_top, (-8, 4))
+        frame_red.paste(pihole_logo_bottom, (-8, 4))
+        draw_red = ImageDraw.Draw(frame_red)
+        draw_red.text((10, height - 17), "Pi", font=font_title, fill=fill_color)
+        draw_red.text(  (23, height - 17), "-hole", font=font_title_bold, fill=fill_color)
+
+        draw_black.text((xt, top + 0), "HOST: ", font=font_bold, fill=fill_color)
+        draw_black.text((xc, top + 0), host, font=font, fill=fill_color)
+        draw_black.text((xt, top + 15), "IP: ", font=font_bold, fill=fill_color)
+        draw_black.text((xc, top + 15), str(ip), font=font, fill=fill_color)
+        draw_black.text((xt, top + 30), "Mem:",  font=font_bold, fill=fill_color)
+        draw_black.text((xc, top + 30), str(mem_usage),  font=font, fill=fill_color)
+        draw_black.text((xt, top + 45), "Disk:",  font=font_bold, fill=fill_color)
+        draw_black.text((xc, top + 45),  str(disk),  font=font, fill=fill_color)
+        draw_black.text((xt, top + 60), "Ads Blocked: ", font=font_bold, fill=fill_color)
+        draw_black.text((xc2, top + 60), str(adsblocked), font=font, fill=fill_color)
+        draw_black.text((xt, top + 75), "Clients:", font=font_bold, fill=fill_color)
+        draw_black.text((xc2, top + 75), str(clients), font=font, fill=fill_color)
+        draw_black.text((xt, top + 90), "DNS Queries: ", font=font_bold, fill=fill_color)
+        draw_black.text((xc2, top + 90), str(dnsqueries), font=font, fill=fill_color)
+
+        epd.display_frame(epd.get_frame_buffer(frame_black.transpose(PIL.Image.ROTATE_90)),
+                        epd.get_frame_buffer(frame_red.transpose(PIL.Image.ROTATE_90)))
+        print "sleeping"
+        epd.sleep()
+        epd.delay_ms(60 * 1000)
+
+def main():
+    print "initing screen..."
+    epd = epd2in13b.EPD()
+    epd.init()
+    try:
+        update(epd)
+    finally:
+        print "sleeping before leaving"
+        epd.sleep()
+
+if __name__ == '__main__':
+    main()
